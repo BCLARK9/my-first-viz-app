f86b69daf8c89988695eb2efa76931fb39d6eb0c
Matthew Turk <matthewturk@gmail.com>
2024-05-22 14:28:37 -0500

    Explore some minor d3 stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import * as d3 from 'd3';
+
 	let myName = 'Matt';
 	let width = 300;
 	let height = 300;
@@ -7,11 +9,13 @@
 	let newcolor = '#AABBCC';
 
 	let myData = [
-		{ x: 1.6, y: 4.0, color: 'blue', colorb: 'red' },
-		{ x: 1.1, y: 0.5, color: 'green', colorb: 'blue' },
-		{ x: 2.1, y: 1.9, color: 'fuchsia', colorb: 'steelblue' },
-		{ x: 4.8, y: 3.1, color: 'steelblue', colorb: 'fuchsia' }
+		{ x: 1.6, y: 4.0, colors: ['red', 'blue', 'green'] },
+		{ x: 1.1, y: 0.5, colors: ['green', 'red', 'blue'] },
+		{ x: 2.1, y: 1.9, colors: ['bluesteel', 'redbrick', 'brown'] },
+		{ x: 4.8, y: 3.1, colors: ['yellow', 'grey', 'black'] }
 	];
+
+	let chosenColorIndex = 0;
 
 	function addNewDatapoint(event: e) {
 		const newPoint = {
@@ -22,13 +26,45 @@
 		};
 		myData = [newPoint, ...myData];
 	}
+
+	let totalArea: number;
+	$: {
+		totalArea = width * height;
+		console.log('Changing totalArea to ', totalArea);
+	}
+
+	let doubleWidth: number;
+
+	$: {
+		doubleWidth = width * 2;
+		console.log('Changing doubleWidth to ', doubleWidth);
+	}
+
+	$: {
+		console.log('Changing chosenColorIndex to ', chosenColorIndex);
+		d3.select('#mySvg')
+			.selectAll('circle')
+			.data(myData)
+			.transition()
+			.style('fill', (d) => d.colors[chosenColorIndex]);
+	}
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>Hi there, {myName}!</p>
 
+<p>
+	Total pixels available: {totalArea}. And, double width is {doubleWidth}. Chosen index is {chosenColorIndex}.
+</p>
 <p><input type="text" bind:value={myName} /></p>
+<p>
+	<select bind:value={chosenColorIndex}>
+		<option value="0">First</option>
+		<option value="1">Second</option>
+		<option value="2">Third</option>
+	</select>
+</p>
 <p>
 	<input type="range" min="100" max="500" step="1" name="width" bind:value={width} /><label
 		for="width">SVG Width</label
@@ -40,7 +76,7 @@
 	>
 </p>
 
-<svg {width} {height} style="border: 1px solid black;">
+<svg id="mySvg" {width} {height} style="border: 1px solid black;">
 	{#each myData as dataPoint}
 		<circle
 			cx={(dataPoint.x * width) / 5.0}
